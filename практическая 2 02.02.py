7e811bae57031a70d406eee19cb27d7a77f26323
jalkiechev <157492311+jalkiechev@users.noreply.github.com>
2024-02-02 12:04:36 +0300

    Add files via upload

---
+++ практическая 2 02.02.py
@@ -0,0 +1,13 @@
+a = 11
+b = 3
+lo = (a>b) and (b<a)
+kak = (a<=b) and (b>=a)
+privet = (a>b) or (b<a)
+lox = (a<b) or (b>a)
+print(lo,kak,privet,lox)
+srt1 = "a"
+srt2 = "b"
+kakak = a or b == a
+print(kakakkak) < b and b == a
+print(kakak)
+kakakkak = a < b
