2c340b05fce8c7be245f24662a69984c2d85cd0a
jalkiechev <157492311+jalkiechev@users.noreply.github.com>
2024-02-16 10:00:29 +0300

    Add files via upload
    классная работа 16.02

---
+++ main (9).py
@@ -0,0 +1,28 @@
+#Задача1
+pop=int(input("введите число"))
+if pop>0
+print("используйте принт")
+elif pop<0:
+    print("-1")
+    else:
+        print("1")
+#Задача2
+pop=int(input("введите число"))
+if pop>0:
+    pop/100
+    print(pop,"введите меньше")
+    elif pop<0:
+        pop+123123132
+        print(pop,"введите больше")
+        else:
+            print("0")
+#Задача3
+pop=int(input("ввод первой переменной"))
+kop=int(input("ввод второй переменной"))
+if pop>kop
+sop=pop-kop
+print(sop)
+elif pop<kop:
+    top=pop+kop
+    print(top)
+
