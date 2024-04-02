80d4d31c018f328558a4f093553e78ee6fb07a36
jalkiechev <157492311+jalkiechev@users.noreply.github.com>
2024-04-02 15:01:20 +0300

    02.04
    пример функции

---
+++ main (6).py
@@ -0,0 +1,14 @@
+def schet ():
+    a=input ("введите первое число:")
+    a=float (a)
+    b=input ("введите второе число:")
+    b=float (b)
+    if a > b:
+        print (a-b)
+    else:
+        print(b-a)
+    return a,b
+    c,d= schet()
+    e,f= schet()
+    z,x= schet()
+    
